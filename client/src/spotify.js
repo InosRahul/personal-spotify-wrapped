756676dbdd387713c63468924b7aedd341ac21a6
Rahul Soni <rahulsoni2225@gmail.com>
2021-10-12 21:33:19 +0530

    initial commit

---
+++ client/src/spotify.js
@@ -0,0 +1,101 @@
+import axios from "axios";
+
+const LOCALSTORAGE_KEYS = {
+    accessToken: 'spotify_access_token',
+    refreshToken: 'spotify_refresh_token',
+    expireTime: 'spotify_token_expire_time',
+    timeStamp: 'spotify_token_timestamp'
+}
+
+const LOCALSTORAGE_VALUES = {
+    accessToken: window.localStorage.getItem(LOCALSTORAGE_KEYS.accessToken),
+    refreshToken: window.localStorage.getItem(LOCALSTORAGE_KEYS.refreshToken),
+    expireTime : window.localStorage.getItem(LOCALSTORAGE_KEYS.expireTime),
+    timeStamp : window.localStorage.getItem(LOCALSTORAGE_KEYS.timeStamp),
+
+}
+
+
+
+const hasTokenExpired = () => {
+    const { accessToken, timestamp, expireTime } = LOCALSTORAGE_VALUES;
+    if (!accessToken || !timestamp) {
+      return false;
+    }
+    const millisecondsElapsed = Date.now() - Number(timestamp);
+    return (millisecondsElapsed / 1000) > Number(expireTime);
+};
+
+export const logout = () => {
+    for (const property in LOCALSTORAGE_KEYS) {
+      window.localStorage.removeItem(LOCALSTORAGE_KEYS[property]);
+    }
+   
+    window.location = window.location.origin;
+  };
+
+const refreshToken = async () => {
+    try {
+      if (!LOCALSTORAGE_VALUES.refreshToken ||
+        LOCALSTORAGE_VALUES.refreshToken === 'undefined' ||
+        (Date.now() - Number(LOCALSTORAGE_VALUES.timestamp) / 1000) < 1000
+      ) {
+        console.error('No refresh token available');
+        logout();
+      }
+  
+    
+      const { data } = await axios.get(`/refresh_token?refresh_token=${LOCALSTORAGE_VALUES.refreshToken}`);
+  
+      // Update localStorage values
+      window.localStorage.setItem(LOCALSTORAGE_KEYS.accessToken, data.access_token);
+      window.localStorage.setItem(LOCALSTORAGE_KEYS.timestamp, Date.now());
+  
+      // Reload the page for localStorage updates to be reflected
+      window.location.reload();
+  
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+const getAccessToken = () => {
+    const query = new URLSearchParams(window.location.search)
+    const queryParams = {
+        [LOCALSTORAGE_KEYS.accessToken]: query.get('access_token'),
+        [LOCALSTORAGE_KEYS.refreshToken]: query.get('refresh_token'),
+        [LOCALSTORAGE_KEYS.expireTime]: query.get('expires_in')
+    }
+
+    const hasError = query.get('error')
+
+    if(hasError || hasTokenExpired() || LOCALSTORAGE_VALUES.accessToken === 'undefined'){
+        refreshToken()
+    }
+
+
+    if(LOCALSTORAGE_VALUES.accessToken && LOCALSTORAGE_VALUES.accessToken !== 'undefined'){
+        return LOCALSTORAGE_VALUES.accessToken
+    }
+    
+    if(queryParams[LOCALSTORAGE_KEYS.accessToken]){
+        for(const property in queryParams){
+            window.localStorage.setItem(property, queryParams[property])
+        }
+        
+        window.localStorage.setItem(LOCALSTORAGE_KEYS.timeStamp, Date.now())
+
+        return queryParams[LOCALSTORAGE_KEYS.accessToken]
+    }
+
+    return false
+}
+
+export const accessToken = getAccessToken();
+
+axios.defaults.baseURL = 'https://api.spotify.com/v1';
+axios.defaults.headers['Authorization'] = `Bearer ${accessToken}`;
+axios.defaults.headers['Content-Type'] = 'application/json';
+
+
+export const getCurrentUserProfile = () => axios.get('/me')
