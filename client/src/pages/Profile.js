2da8d096db669753258f38ee0994409f8237f59b
Rahul Soni <rahulsoni2225@gmail.com>
2021-10-13 11:05:57 +0530

    add: Profile header

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,27 +1,45 @@
 import { useState, useEffect } from 'react';
-import { getCurrentUserProfile } from '../spotify';
 import { catchErrors } from '../utils';
+import { getCurrentUserProfile } from '../spotify';
+import { StyledHeader } from '../styles';
 
 const Profile = () => {
   const [profile, setProfile] = useState(null);
+
   useEffect(() => {
     const fetchData = async () => {
       const { data } = await getCurrentUserProfile();
       setProfile(data);
     };
+
     catchErrors(fetchData());
   }, []);
 
   return (
     <>
       {profile && (
-        <div>
-          <h1>{profile.display_name}</h1>
-          <p>{profile.followers.total} Followers</p>
-          {profile.images.length && profile.images[0].url && (
-            <img src={profile.images[0].url} alt="Avatar" />
-          )}
-        </div>
+        <>
+          <StyledHeader type="user">
+            <div className="header__inner">
+              {profile.images.length && profile.images[0].url && (
+                <img
+                  className="header__img"
+                  src={profile.images[0].url}
+                  alt="Avatar"
+                />
+              )}
+              <div>
+                <h1 className="header__name">{profile.display_name}</h1>
+                <p className="header__meta">
+                  <span>
+                    {profile.followers.total} Follower
+                    {profile.followers.total !== 1 ? 's' : ''}
+                  </span>
+                </p>
+              </div>
+            </div>
+          </StyledHeader>
+        </>
       )}
     </>
   );
