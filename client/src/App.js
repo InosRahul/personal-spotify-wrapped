e96533c31a2abecb23f6bc753c0886dfb5ae873c
Rahul Soni <rahulsoni2225@gmail.com>
2021-10-13 10:43:28 +0530

    Fix: Logout button done

--- client/src/App.js
+++ client/src/App.js
@@ -1,70 +1,61 @@
 import { useState, useEffect } from 'react';
-import { accessToken, getCurrentUserProfile } from './spotify';
-import { catchErrors } from './utils';
+import { accessToken, logout } from './spotify';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import styled from 'styled-components/macro';
 import { GlobalStyle } from './styles';
+import { Login, Profile } from './pages';
+import styled from 'styled-components/macro';
 
-const StyledLoginButton = styled.a`
-  background-color: green;
-  color: white;
-  padding: 10px 20px;
-  display: inline-block;
-  margin: 20px auto;
-  border-radius: 30px;
+const StyledLogoutBtn = styled.a`
+  position: absolute;
+  top: var(--spacing-sm);
+  right: var(--spacing-md);
+  padding: var(--spacing-xs) var(--spacing-sm);
+  background-color: rgba(0, 0, 0, 0.7);
+  color: var(--white);
+  font-size: var(--fz-sm);
+  font-weight: 700;
+  border-radius: var(--border-radius-pill);
+  z-index: 10;
+  @media (min-width: 768px) {
+    right: var(--spacing-lg);
+  }
 `;
 
 function App() {
   const [token, setToken] = useState(null);
-  const [profile, setProfile] = useState(null);
   useEffect(() => {
     setToken(accessToken);
-
-    const fetchData = async () => {
-      const { data } = await getCurrentUserProfile();
-      setProfile(data);
-    };
-    catchErrors(fetchData());
   }, []);
   return (
     <div className="App">
       <GlobalStyle />
       <header className="App-header">
         {!token ? (
-          <StyledLoginButton href="http://localhost:8888/login">
-            Log into Spotify
-          </StyledLoginButton>
+          <Login />
         ) : (
-          <Router>
-            <Switch>
-              <Route path="/top-artists">
-                <h1>Top Artists</h1>
-              </Route>
-              <Route path="/top-tracks">
-                <h1>Top Tracks</h1>
-              </Route>
-              <Route path="/playlists/:id">
-                <h1>Playlist</h1>
-              </Route>
-              <Route path="/playlists">
-                <h1>Playlists</h1>
-              </Route>
-              <Route path="/">
-                <>
-                  {profile && (
-                    <div>
-                      <h1>{profile.display_name}</h1>
-                      <p>{profile.followers.total} Followers</p>
-                      {profile.images.length && profile.images[0].url && (
-                        <img src={profile.images[0].url} alt="Avatar" />
-                      )}
-                    </div>
-                  )}
-                </>
-              </Route>
-            </Switch>
-          </Router>
+          <>
+            <StyledLogoutBtn onClick={logout}>Log Out</StyledLogoutBtn>
+            <Router>
+              <Switch>
+                <Route path="/top-artists">
+                  <h1>Top Artists</h1>
+                </Route>
+                <Route path="/top-tracks">
+                  <h1>Top Tracks</h1>
+                </Route>
+                <Route path="/playlists/:id">
+                  <h1>Playlist</h1>
+                </Route>
+                <Route path="/playlists">
+                  <h1>Playlists</h1>
+                </Route>
+                <Route path="/">
+                  <Profile />
+                </Route>
+              </Switch>
+            </Router>
+          </>
         )}
       </header>
     </div>
